0efe7d826a25e1186747c1037afb68fb919675cb
omar <oabdelnasser877@gmail.com>
2024-07-21 17:28:32 +0300

    first commit

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-LXUQO33O.mjs";import"./chunk-NZLD2TYU.mjs";import"./chunk-ZKF7UF3S.mjs";import"./chunk-XV4GN4YB.mjs";import"./chunk-E4T3BF62.mjs";import"./chunk-SSCMCJS2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path : 'budget-planner' , loadChildren :()=> import('./budget-planner/budget-planner.module').then(m => m.BudgetPlannerModule)},
+  {path : '' , pathMatch:'full' , redirectTo : 'budget-planner'}
+];
